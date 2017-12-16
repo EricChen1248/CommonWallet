a7a2df9dba45412e7b2a4118da0db6f83a0090b9
Eric Chen <19816842+EricChen1248@users.noreply.github.com>
2017-12-16 16:17:42 +0800

    Added Helper Function: GetParent of type T

--- CommonWallet/Class/Helper.cs
+++ CommonWallet/Class/Helper.cs
@@ -1,6 +1,20 @@
-﻿namespace CommonWallet.Class
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using CommonWallet.UserControls;
+
+namespace CommonWallet.Class
 {
     internal static class Helper
     {
+        public static object GetParent<T>(DependencyObject sender)
+        {
+            while (true)
+            {
+                var result = VisualTreeHelper.GetParent(sender ?? throw new ArgumentNullException(nameof(sender)));
+                if (result is T) return result ;
+                sender = result;
+            }
+        }
     }
 }
