fe973f8ca52557f1df80ba11e29cb574db024dd0
Eric Chen <19816842+EricChen1248@users.noreply.github.com>
2017-12-16 18:04:13 +0800

    Added helper function Bitmap To ImageSource

--- CommonWallet/Class/Helper.cs
+++ CommonWallet/Class/Helper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using CommonWallet.UserControls;
 
 namespace CommonWallet.Class
@@ -14,6 +17,21 @@ namespace CommonWallet.Class
                 var result = VisualTreeHelper.GetParent(sender ?? throw new ArgumentNullException(nameof(sender)));
                 if (result is T) return result ;
                 sender = result;
+            }
+        }
+        public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                var bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
             }
         }
     }
