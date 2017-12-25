76f63554ca2fe2e2846490ddabb5ec6a6711a504
Eric Chen <19816842+EricChen1248@users.noreply.github.com>
2017-12-25 22:42:36 +0800

    Corrected ambiguous name in WalletUserData
    AccountName changed to UserName. Added link to database to old name.

--- CommonWallet/DataClasses/WalletUserData.cs
+++ CommonWallet/DataClasses/WalletUserData.cs
@@ -1,9 +1,12 @@
-﻿namespace CommonWallet.DataClasses
+﻿using LiteDB;
+
+namespace CommonWallet.DataClasses
 {
     public class WalletUserData
     {
         public int ID { get; set; }
-        public string AccountName { get; set; }
+        [BsonField("AccountName")]
+        public string UserName { get; set; }
         public string WalletGuid { get; set; }
     }
 }
